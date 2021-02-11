07c650d9b71372716ec3ba396ac367dac36ec47c
tomm-he <78884758+tomm-he@users.noreply.github.com>
2021-02-11 00:25:23 +0000

    Update and rename music.py to help.py

--- cogs/help.py
+++ cogs/help.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 
 
 
-class Music(commands.Cog):
+class help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -60,4 +60,4 @@ class Music(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Music(bot))
+    bot.add_cog(help(bot))
