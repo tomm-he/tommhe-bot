eb57de7f51d3eafd05bd0eab0395cc6b325ba84f
tomm-he <78884758+tomm-he@users.noreply.github.com>
2021-04-22 23:05:28 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -4,10 +4,14 @@ import discord
 import random
 import sqlite3
 import datetime
+from pyowm import OWM
 
 from discord.ext import commands
 
 extensions = ["cogs.help","cogs.boxing","cogs.boxingnames","cogs.match","cogs.motogp","cogs.motogpnames","cogs.names","cogs.nba","cogs.nhl","cogs.nhlnames","cogs.ufc","cogs.ufcnames","cogs.f1","cogs.f1names","cogs.nfl","cogs.nflnames"]
+
+owm = OWM('OWM_TOKEN') # Gain the token at https://home.openweathermap.org/users/sign_up
+mgr = owm.weather_manager()
 
 
 
@@ -27,6 +31,7 @@ class help(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_guild=True)
     async def prefix(self, ctx, prefix):
+        """custom prefix"""
         if not prefix:
             return await ctx.send("You Did Not Mention a Prefix!")
         list = self.bot.cur.execute(f"SELECT * FROM prefix WHERE guild={ctx.guild.id}").fetchall()
@@ -61,6 +66,7 @@ class help(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def reload(self,ctx):
+        """reload all cogs"""
         try:
             for cogs in extensions:
                 self.bot.reload_extension(cogs)
@@ -74,6 +80,67 @@ class help(commands.Cog):
             em = discord.Embed(color=random.randint(0, 0xFFFFFF))
             em.description = f"{e.__class__.__name__}: {e}"
             await ctx.send(embed=em)
+            
+    @commands.command(aliases=["weather","temp"])
+    async def weathercheck(self,ctx, *, city):
+        """quick weather report on any location, city, country, town. OWM token needed."""
+        observation = mgr.weather_at_place(city)
+        w = observation.weather
+        
+        try:
+            fmt = "%H:%M %p" # "**%d-%m-%Y** **%H:%M %p** %Z%z"
+        
+            clouds = w.detailed_status                          # 'clouds report'
+            wind = w.wind()["speed"]                            # {'deg': 330}
+            humidity = w.humidity                               
+            tempmax  = w.temperature('celsius')["temp_max"]
+            temp = w.temperature('celsius')["temp"]
+            tempmin = w.temperature('celsius')["temp_min"]
+            feelslike = w.temperature('celsius')["feels_like"]  
+            cloudpercent = w.clouds
+            sunrise_datetime = w.sunrise_time(timeformat='date')
+            sunrise = sunrise_datetime.strftime(fmt)
+            sunset_datetime = w.sunset_time(timeformat='date')
+            sunset = sunset_datetime.strftime(fmt)
+            timestamp = timestamps.now()
+            time = timestamp.strftime(fmt)
+
+            c = "°C"
+
+            rain = None
+            if rain is not None:
+                rain = w.rain
+
+            if temp > 11:
+                image = "https://cdn.discordapp.com/attachments/822851842715287622/834536500111736883/sun-behind-cloud_26c5.png"
+            elif temp > 17:
+                image = "https://cdn.discordapp.com/attachments/822851842715287622/834537180394815519/The_Sun_Emoji_grande.png"
+            else:
+                image = "https://cdn.discordapp.com/attachments/822851842715287622/834537887499681852/f74df038c7948001f079960b1c27d63a-cloudy-icon-by-vexels.png"
+
+            
+            em = discord.Embed(color=random.randint(0, 0xFFFFFF),title=f"*Weather Rreport For {city}*",description = f":cloud: **{clouds}**")
+            em.set_author(name=ctx.author,icon_url=ctx.author.avatar_url)
+            em.set_thumbnail(url=image)
+            em.timestamp = datetime.datetime.utcnow()
+            em.add_field(name=f"Temperature :white_sun_small_cloud:",value=f"[``{temp}{c}``]",inline=True)
+            em.add_field(name=f"Max Temperature :heavy_plus_sign:",value=f"[``{tempmax}{c}``]",inline=True)
+            em.add_field(name=f"Min Temperature: :heavy_minus_sign:",value=f"[``{tempmin}{c}``]",inline=True)
+            em.add_field(name=f"Feels Like: :fog:",value=f"[``{feelslike}{c}``]",inline=True)
+            em.add_field(name=f"Clouds %: :white_sun_rain_cloud:",value=f"[``{cloudpercent}%``]",inline=True)
+            em.add_field(name=f"Rain: :cloud_rain:",value=f"[``{rain}``]",inline=True)
+            em.add_field(name=f"Humidity: :earth_americas:",value=f"[``{humidity}%``]",inline=True)
+            em.add_field(name=f"Sunrise: :sunrise_over_mountains:",value=f"[``{sunrise}``]",inline=True)
+            em.add_field(name=f"Sunset: :city_sunset:",value=f"[``{sunset}``]")
+
+            await ctx.message.delete()
+
+            await ctx.send(embed=em)
+
+        except Exception:
+            e = discord.Embed(color=random.randint(0, 0xFFFFFF))
+            em.description = f"The Location Parameter {city} Was Wrong, and I Could Not Grab Data For That. Please Try Again"
+            await ctx.reply(embed=e) 
 
 
 def setup(bot):
